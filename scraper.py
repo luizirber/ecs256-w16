6b479e7ac0188cde67d19e2e19c8a31dbda321b5
Luiz Irber <luiz.irber@gmail.com>
2016-01-10 12:07:51 -0800

    parse page, include dateutil dependency

--- scraper.py
+++ scraper.py
@@ -1,24 +1,30 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import dateutil.parser
+import lxml.html
+import scraperwiki
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# Read in a page
+html = scraperwiki.scrape("http://heather.cs.ucdavis.edu/~matloff/256/Blog.html")
+
+# Find something on the page using css selectors
+root = lxml.html.fromstring(html)
+for item in root.cssselect("h2"):
+    content = []
+    current_item = item.getnext()
+    while current_item is not None and current_item.tag != "h2":
+        content.append(lxml.etree.tostring(current_item))
+        current_item = current_item.getnext()
+
+    data = {
+      'title': item.text,
+      'content': "".join(content),
+      'link': "http://heather.cs.ucdavis.edu/~matloff/256/Blog.html",
+      'date': dateutil.parser.parse(item.text)
+    }
+    # Write out to the sqlite database using scraperwiki library
+    scraperwiki.sqlite.save(unique_keys=['title'], data=data)
+
+# An arbitrary query against the database
+#scraperwiki.sql.select("* from data where 'name'='peter'")
